422507e85dbfa02acea227a2207346e6e5d3a441
Carmeldj <carmeldjegui01@gmail.com>
2022-10-18 15:29:56 +0100

    Adding 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *_abs - compute the absolute of a integer
+  *@int: The variable xe find absolute
+  *Return: return 0
+  */
+int _abs(int n)
+{
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+	return (0);
+}
