ccc5daf16ebebaa9611183a92d09524c47c5f90b
Carmeldj <carmeldjegui01@gmail.com>
2022-10-24 11:29:35 +0100

    Adding 4-print_rev.c file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,17 @@ void print_rev(char *s)
 {
 	int i, j, l;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+
 	l = i;
+
 	for (j = l - 1; j >= 0; j--)
 	{
+
 	_putchar(s[j]);
 	}
+
 	_putchar('\n');
 }
