501e33c42865b749bcd62e6f5f24ea8202eaf0c9
Carmeldj <carmeldjegui01@gmail.com>
2022-11-10 17:16:55 +0100

    Adding 0-mallo_checker.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - Allocates memory using malloc.
- * @b: The number of bytes to be allocated.
+ * malloc_checked - Allocates memory using malloc
+ * @b: The number of bytes to be allocated
  *
- * Return: A pointer to the allocated memory.
+ * Return: A pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-void *m = malloc(b);
+	void *m;
 
-if (m == NULL)
-exit(98);
+	m = malloc(b);
 
-return (m);
+	if (m == NULL)
+		exit(98);
+
+	return (m);
 }
